5ec3f1cbd7df38cce9efb81d4b298cec3f1f8411
pdbro2k <40125493+pdbro2k@users.noreply.github.com>
2025-05-22 10:14:10 +0200

    introduced !XML flag to $$IMPORT$$

--- src/main/java/org/adwmainz/da/extensions/importmore/utils/ImportMoreAnnotationParser.java
+++ src/main/java/org/adwmainz/da/extensions/importmore/utils/ImportMoreAnnotationParser.java
@@ -6,6 +6,7 @@
  */
 package org.adwmainz.da.extensions.importmore.utils;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -19,23 +20,43 @@ import net.sf.saxon.s9api.XdmNode;
 public class ImportMoreAnnotationParser {
 	
 	// relevant regex pattern
-	protected static final String IMPORT_MORE_ANNOTATION_PATTERN = "\\$\\$IMPORT" + "\\(" + "(.*?)" /* XPath */ + "\\)" + "\\$\\$";
+	protected static final String IMPORT_MORE_ANNOTATION_PATTERN = "\\$\\$IMPORT" + "\\(" + "(.*?)" /* XPath */ + "\\)" + "(!.*)?" /* Flags */ + "\\$\\$";
+	protected static final String FLAG_SEPARATOR = "!";
 	
 	/**
 	 * Replaces all ImportMoreAnnotations in a given String with XPath results from the given context node
 	 * @param annotatedText a String that may contain serialized annotations
 	 * @param contextNode the XdmNode serving as the context for XPath expressions within the given ImportMoreAnnotations
-	 * @throws ImportMoreXMLException if an XML parsing error occurrs
-	 * @throws ImportMoreXPathException if an XPath parsing error occurrs
+	 * @param namespaceMap a Map of namespace prefixes and their respective namespace URIs
+	 * @throws ImportMoreXMLException if an XML parsing error occurs
+	 * @throws ImportMoreXPathException if an XPath parsing error occurs
 	 */
 	public static String replaceAnnotations(String annotatedText, XdmNode contextNode, Map<String, String> namespaceMap)
 			throws ImportMoreXMLException, ImportMoreXPathException {
-		for (String importMoreAnnotation: new HashSet<>(findAnnotations(annotatedText))) {
+		for (String importMoreAnnotation: new HashSet<>(ImportMoreAnnotationParser.findAnnotations(annotatedText))) {
 			String xPathExpression = RegexUtils.getFirstMatch(importMoreAnnotation, IMPORT_MORE_ANNOTATION_PATTERN, 1);
-			String importedFragment = SaxonUtils.getFirstXPathResult(contextNode, xPathExpression, namespaceMap).getStringValue();
+			List<String> flags = getFlags(importMoreAnnotation);
+			
+			String importedFragment = "";
+			if (flags.contains("XML"))
+				// concat all result items
+				importedFragment = String.join("", SaxonUtils.getSerializedXPathResults(contextNode, xPathExpression, namespaceMap));
+			else
+				// get only the text value of the first result item
+				importedFragment = SaxonUtils.getFirstXPathResult(contextNode, xPathExpression, namespaceMap).getStringValue();			
 			annotatedText = annotatedText.replace(importMoreAnnotation, importedFragment);
 		}
 		return annotatedText;
+	}
+	
+	protected static List<String> getFlags(String importMoreAnnotation) {
+		List<String> flags = new ArrayList<>();
+		String serializedFlags = RegexUtils.getFirstMatch(importMoreAnnotation, IMPORT_MORE_ANNOTATION_PATTERN, 2);
+		if (serializedFlags != null) {
+			for (String flag: serializedFlags.split(FLAG_SEPARATOR))
+				flags.add(flag);
+		}
+		return flags;
 	}
 	
 	/**
@@ -51,6 +72,8 @@ public class ImportMoreAnnotationParser {
 	 */
 	public static String getDescription() {
 		return "You may import one or more parts of this argument by using the annotation $$IMPORT( xpath )$$ where xpath denotes an XPath expression relative to"
-				+ " the result(s) of the param '" + ImportMoreArgumentProvider.ARGUMENT_REQUESTED_ELEMENT_LOCATION + "'.";
+				+ " the result(s) of the param '" + ImportMoreArgumentProvider.ARGUMENT_REQUESTED_ELEMENT_LOCATION + "'.\n"
+				+ "Please note that this will only return the String value of the first XPath result! However, you can also use $$IMPORT( xpath )!XML$$ to get a"
+				+ " fragment consisting of all results and their respective markup instead.";
 	}
 }
